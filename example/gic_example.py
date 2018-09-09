b00d097e0660cf5ceae61044c4fbf61bc283fdbc
MathieuCarriere <mathieu.carriere3@gmail.com>
2018-09-09 15:30:57 -0400

    small change on gic example

--- example/gic_example.py
+++ example/gic_example.py
@@ -3,7 +3,7 @@ import sklearn_tda as tda
 
 X = np.loadtxt("human")
 
-cov = tda.GraphInducedComplex(cover_type = "functional", filter = 0).fit(X)
+cov = tda.GraphInducedComplex(cover_type = "functional", filter = 2).fit(X)
 cov.print_result("dot")
 
 print(cov.compute_confidence_level_from_distance(bootstrap = 20, distance = 0.2))
