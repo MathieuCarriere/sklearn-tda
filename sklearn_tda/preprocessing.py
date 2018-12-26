31c59478628fbe4ed9f7a97dd38c3cc26d4e5f58
mathieu <mathieu.carriere3@gmail.com>
2018-12-26 16:06:08 -0500

    small fix

--- sklearn_tda/preprocessing.py
+++ sklearn_tda/preprocessing.py
@@ -67,10 +67,10 @@ class ProminentPoints(BaseEstimator, TransformerMixin):
             for i in range(num_diag):
                 diag = X[i]
                 if diag.shape[0] > 0:
-                    pers       = np.matmul(diag, [-1.0, 1.0])
+                    pers       = np.matmul(diag[:,:2], [-1.0, 1.0])
                     idx_thresh = pers >= self.threshold
                     thresh_diag, thresh_pers  = diag[idx_thresh.flatten()], pers[idx_thresh.flatten()]
-                    sort_index  = np.flip(np.argsort(thresh_pers, axis = None),0)
+                    sort_index  = np.flip(np.argsort(thresh_pers, axis=None), 0)
                     if self.point_type == "upper":
                         new_diag = thresh_diag[sort_index[:min(self.num_pts, thresh_diag.shape[0])],:]
                     if self.point_type == "lower":
